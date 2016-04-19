5d79f5890ef82741608e6a745b59816cf8a8a9f1
Emily Helfer <emilyhelfer1@Emilys-MacBook-Air.local>
2016-04-18 23:41:26 -0400

    update algo2

--- paperAlgo2.py
+++ paperAlgo2.py
@@ -11,24 +11,20 @@ import numpy
 Gm = 0
 gm = np.zeros(len(feature[0]))
 
-def algo2(i, m, Um, b, groups, feature):
+def algo2(i, m, Um, b, groups, feature, t):
 	global Gm
 	global gm
 
 	s = 1
-	links = 0
-	total = 0
 	i_group = NULL
 	for group in range(nrow(groups)):
-		links += (len(groups[group])-1)*len(groups[group])
-		total += len(groups[group])
 		for item in groups[group]:
 			if item == i: 
 				i_group = group
 			feat = feature[item]
 			Gm += np.dot(feat.transpose(),feat)
 			gm += feat
-	tm = float(links) / (total * (total-1))
+	tm = t[m]
 	sameSum = 0
 	featureSum = np.zeros(len(feature[0]))
 	for elem in groups[i_group]:
